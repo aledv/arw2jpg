7e44eb37b24e2f25b86a13f93438f2f1d0e8212b
Alessandro Di Vanni <alessandro.divanni@gmail.com>
2019-10-03 15:18:27 +0200

    initial commit

---
+++ arw2jpg.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import rawpy
+import imageio
+import glob
+import os
+import sys
+
+files = glob.glob(sys.argv[1]+'*.ARW')
+num_photos = len(files)
+print ('nr ARW founded -->' + str(num_photos))
+
+if num_photos > 0:
+
+    if not os.path.exists(sys.argv[1] + 'converted/'):
+        os.makedirs(sys.argv[1] + 'converted/')
+
+    for f in files:
+        print ('Processing -->' + f)
+        with rawpy.imread(f) as raw:
+            rgb = raw.postprocess()
+        fileName = os.path.splitext(f)[0] + '.jpg'
+        fileName = fileName.replace(sys.argv[1],'')
+        newFileName = sys.argv[1] + 'converted/' + fileName
+        imageio.imsave(newFileName, rgb)
+
+print('Conversion finished')
+
